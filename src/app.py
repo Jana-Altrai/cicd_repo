96a9121a1ea5a0fd468d7f7f7e172766516f515e
Jana-Altrai <janagan@altrai.in>
2024-12-10 17:52:32 +0530

    updated status code in 204.

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/health", methods=["GET"])
 def health_check():
-    return jsonify({"status": "ok"}), 201
+    return jsonify({"status": "ok"}), 204
 
 
 # The new endpoint you’re adding on the feature branch
